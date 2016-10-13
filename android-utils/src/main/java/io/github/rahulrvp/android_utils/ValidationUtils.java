8133b8c8b1f51534743f1c041e0b63f0f77ff6ee
Rahul Raveendran <rahul@bullfin.ch>
2016-10-13 11:17:45 +0530

    added some validation utils

---
+++ android-utils/src/main/java/io/github/rahulrvp/android_utils/ValidationUtils.java
@@ -0,0 +1,90 @@
+package io.github.rahulrvp.android_utils;
+
+import android.telephony.PhoneNumberUtils;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Wraps commonly used form validation methods.
+ *
+ * @author Rahul Raveendran V P
+ *         Created on 11/10/16 @ 4:39 PM
+ *         https://github.com/rahulrvp
+ */
+
+
+public class ValidationUtils {
+
+    /**
+     * Checks if the email is valid.
+     *
+     * @param email email address to be validated.
+     * @return true if valid, false otherwise
+     */
+    public static boolean isEmail(String email) {
+        return !TextUtils.isEmpty(email)
+                && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    /**
+     * Checks if the passed in phone number is valid according to global standards.
+     *
+     * @param phoneNumber the phone number to be validated.
+     * @return true if valid, false otherwise
+     */
+    public static boolean isGlobalPhoneNumber(String phoneNumber) {
+        return !TextUtils.isEmpty(phoneNumber)
+                && PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber);
+    }
+
+    /**
+     * Checks if the phone number is a valid indian phone number.
+     * <p>
+     * Note: This is a very basic validation I added. Any improvements are welcomed.
+     * Please see the implementation for more details.
+     *
+     * @param phoneNumber the phone number to be validated.
+     * @return true if valid, false otherwise
+     */
+    public static boolean isIndianPhoneNumber(String phoneNumber) {
+        boolean result = isGlobalPhoneNumber(phoneNumber);
+
+        if (result) {
+            if (phoneNumber.length() > 10) {
+                result = phoneNumber.length() == 13 && (phoneNumber.startsWith("+91")
+                        || phoneNumber.length() == 11 && phoneNumber.startsWith("0"));
+            } else if (phoneNumber.length() == 10) {
+                result = !phoneNumber.startsWith("0");
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * This method helps to remove all the whitespaces and hyphens added in the phone number
+     * field once the edit text loses it's focus.
+     * <p>
+     * Note: This action will be overridden if the developer adds another focus change listener
+     * after calling this method.
+     *
+     * @param editText The {@link EditText} on which we need to attach the listener.
+     */
+    public static void addPhoneNumberValidator(final EditText editText) {
+        if (editText != null) {
+            editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus) {
+                        String phoneNumber = EditTextUtils.getText(editText);
+                        if (phoneNumber != null) {
+                            phoneNumber = phoneNumber.replaceAll("[ -]", "");
+                            EditTextUtils.setText(editText, phoneNumber);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/io/github/rahulrvp/androidutilslib/MainActivity.java
+++ app/src/main/java/io/github/rahulrvp/androidutilslib/MainActivity.java
@@ -2,6 +2,9 @@ package io.github.rahulrvp.androidutilslib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import io.github.rahulrvp.android_utils.ValidationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onTestClicked(View view) {
+        ValidationUtils.isIndianPhoneNumber("******226");
     }
 }
