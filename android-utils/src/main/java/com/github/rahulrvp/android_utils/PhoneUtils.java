672ab22f951242083898b013405159b8b5314eb9
Rahul Raveendran <rahul@bullfin.ch>
2016-11-07 20:25:16 +0530

    added class javadocs in textview utils and phone utils

--- android-utils/src/main/java/com/github/rahulrvp/android_utils/PhoneUtils.java
+++ android-utils/src/main/java/com/github/rahulrvp/android_utils/PhoneUtils.java
@@ -1,19 +1,17 @@
 /**
- *
  * Copyright 2016 Rahul Raveendran
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.github.rahulrvp.android_utils;
@@ -24,6 +22,8 @@ import android.text.TextUtils;
 import android.util.Log;
 
 /**
+ * Collection of phone related util methods to save lines of code.
+ *
  * @author Rahul Raveendran V P
  *         Created on 3/10/16 @ 7:40 PM
  *         https://github.com/rahulrvp
--- android-utils/src/main/java/com/github/rahulrvp/android_utils/TextViewUtils.java
+++ android-utils/src/main/java/com/github/rahulrvp/android_utils/TextViewUtils.java
@@ -1,19 +1,17 @@
 /**
- *
  * Copyright 2016 Rahul Raveendran
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.github.rahulrvp.android_utils;
@@ -23,6 +21,8 @@ import android.util.Log;
 import android.widget.TextView;
 
 /**
+ * Abstracts the commonly used methods on an {@link TextView}
+ *
  * @author Rahul Raveendran V P
  *         Created on 13/10/16 @ 7:05 PM
  *         https://github.com/rahulrvp
