a5e3218c6a6e938598d744ce72412d107ddd3488
Rahul Raveendran <rahul@bullfin.ch>
2017-02-08 12:20:46 +0530

    corrected a bug inside indian phone number validator

--- android-utils/src/main/java/com/github/rahulrvp/android_utils/ValidationUtils.java
+++ android-utils/src/main/java/com/github/rahulrvp/android_utils/ValidationUtils.java
@@ -1,19 +1,17 @@
 /**
- *
  * Copyright 2016 Rahul Raveendran
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.github.rahulrvp.android_utils;
@@ -74,10 +72,11 @@ public class ValidationUtils {
 
         if (result) {
             if (phoneNumber.length() > 10) {
-                result = phoneNumber.length() == 13 && (phoneNumber.startsWith("+91")
-                        || phoneNumber.length() == 11 && phoneNumber.startsWith("0"));
-            } else if (phoneNumber.length() == 10) {
-                result = !phoneNumber.startsWith("0");
+                result = phoneNumber.length() == 11 && phoneNumber.startsWith("0")
+                        || phoneNumber.length() == 13 && phoneNumber.startsWith("+91")
+                        || phoneNumber.length() == 14 && phoneNumber.startsWith("0091");
+            } else {
+                result = phoneNumber.length() == 10 && !phoneNumber.startsWith("0");
             }
         }
 
